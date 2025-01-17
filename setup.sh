b5831447dbda7e10243c09786a23239600f55854
Joshua Beirer <joshua.beirer@cern.ch>
2025-01-17 16:29:29 +0100

    Fix CI

--- setup.sh
+++ setup.sh
@@ -1,1 +1,7 @@
-source /cvmfs/sw.hsf.org/key4hep/setup.sh -r 2024-11-28
+#!/bin/bash
+
+# Use environment variable if set, otherwise use default version
+KEY4HEP_VERSION=${KEY4HEP_VERSION:-"2024-11-28"}
+
+# Setup key4hep environment
+source /cvmfs/sw.hsf.org/key4hep/setup.sh -r "${KEY4HEP_VERSION}"
