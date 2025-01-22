d5670069f1101308c4ecd8db13c3a4c9e7df2d8e
Joshua Beirer <39070576+jbeirer@users.noreply.github.com>
2025-01-22 15:18:50 +0100

    Add linting and spell fixing tools (#3)

--- include/DDFastCaloSim/FastCaloSim.h
+++ include/DDFastCaloSim/FastCaloSim.h
@@ -1,8 +1,9 @@
 #ifndef FastCaloSim_HH
 #define FastCaloSim_HH
 
-#include <G4String.hh>                // for G4String
-#include <G4Types.hh>                 // for G4bool
+#include <G4String.hh>  // for G4String
+#include <G4Types.hh>  // for G4bool
+
 #include "G4VFastSimulationModel.hh"  // for G4VFastSimulationModel
 class G4FastStep;
 class G4FastTrack;
@@ -11,61 +12,74 @@ class G4Region;
 
 #include <DDG4/Geant4FastSimShowerModel.h>
 
+#include <G4FastHit.hh>
 #include <G4FastStep.hh>
 #include <G4FastTrack.hh>
 #include <G4Track.hh>
-#include <G4FastHit.hh>
-
 
 /**
  * @brief Register particles at calorimeter layer
  *
  */
-namespace dd4hep{
-  namespace sim{
-class FastCaloSim :  public dd4hep::sim::Geant4FastSimShowerModel
+namespace dd4hep
 {
- public:
-/** C'tor that calls initialize of the concrete model implementation in order to
-     *  allow for dedicated properties to be declared.
-     */
-    FastCaloSim(dd4hep::sim::Geant4Context* context, const std::string& nam);
-  virtual ~FastCaloSim(){};
+namespace sim
+{
+class FastCaloSim : public dd4hep::sim::Geant4FastSimShowerModel
+{
+public:
+  /** C'tor that calls initialize of the concrete model implementation in order
+   * to allow for dedicated properties to be declared.
+   */
+  FastCaloSim(dd4hep::sim::Geant4Context* context, const std::string& name);
+  virtual ~FastCaloSim() {};
 
- /// Geometry construction callback. Called at "Construct()"
-    virtual void constructGeo(dd4hep::sim::Geant4DetectorConstructionContext* ctxt)  override {
-      this->Geant4FastSimShowerModel::constructGeo(ctxt);
+  /// Geometry construction callback. Called at "Construct()"
+  virtual void constructGeo(
+      dd4hep::sim::Geant4DetectorConstructionContext* ctxt) override
+  {
+    this->Geant4FastSimShowerModel::constructGeo(ctxt);
+  }
 
-    }
+  /// Electromagnetic field construction callback. Called at
+  /// "ConstructSDandField()"
+  virtual void constructField(
+      dd4hep::sim::Geant4DetectorConstructionContext* ctxt) override
+  {
+    this->Geant4FastSimShowerModel::constructField(ctxt);
+  }
 
-    /// Electromagnetic field construction callback. Called at "ConstructSDandField()"
-    virtual void constructField(dd4hep::sim::Geant4DetectorConstructionContext* ctxt)  override {
-      this->Geant4FastSimShowerModel::constructField(ctxt);
-    }
+  /// Sensitive detector construction callback. Called at
+  /// "ConstructSDandField()"
+  virtual void constructSensitives(
+      dd4hep::sim::Geant4DetectorConstructionContext* ctxt) override
+  {
+    this->Geant4FastSimShowerModel::constructSensitives(ctxt);
+  }
 
-    /// Sensitive detector construction callback. Called at "ConstructSDandField()"
-    virtual void constructSensitives(dd4hep::sim::Geant4DetectorConstructionContext* ctxt)  override {
-      this->Geant4FastSimShowerModel::constructSensitives(ctxt);
-    }
+  /// User callback to determine if the model is applicable for the particle
+  /// type
+  /** Default implementation checks if the particle is registered in
+   * 'ApplicableParticles'
+   */
+  virtual bool check_applicability(
+      const G4ParticleDefinition& particle) override
+  {
+    /// this model can be used with all particles
+    return true;
+  }
 
-    /// User callback to determine if the model is applicable for the particle type
-    /** Default implementation checks if the particle is registered in 'ApplicableParticles'
-     */
-    virtual bool check_applicability(const G4ParticleDefinition& particle)  override {
-      /// this model can be used with all particles
-      return true;
-    }
+  /// User callback to determine if the shower creation should be triggered
+  /** Default implementation checks if for all particles registered in
+   * 'Etrigger' the kinetic energy is bigger than the value.
+   */
+  virtual bool check_trigger(const G4FastTrack& track) override;
 
-    /// User callback to determine if the shower creation should be triggered
-    /** Default implementation checks if for all particles registered in 'Etrigger'
-     *  the kinetic energy is bigger than the value.
-     */
-    virtual bool check_trigger(const G4FastTrack& track)  override;
-
-
-    /// User callback to model the particle/energy shower - details defined in ML_MODEL
-    virtual void modelShower(const G4FastTrack& track, G4FastStep& step) override;
+  /// User callback to model the particle/energy shower - details defined in
+  /// ML_MODEL
+  virtual void modelShower(const G4FastTrack& track, G4FastStep& step) override;
 };
-}}
+}  // namespace sim
+}  // namespace dd4hep
 
 #endif /* FastCaloSim_HH */
