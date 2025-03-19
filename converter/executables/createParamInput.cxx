d4b1aaaf803fc33d8c66ae259a137da31248f406
Joshua Beirer <joshua.beirer@cern.ch>
2025-03-19 14:15:51 +0100

    Add basic hit access in converter

--- converter/executables/createParamInput.cxx
+++ converter/executables/createParamInput.cxx
@@ -3,6 +3,7 @@
 #include "FastCaloSim/Core/TFCSExtrapolationState.h"
 #include "FastCaloSimConverter/ExtrapolationConverter.h"
 #include "FastCaloSimConverter/InputLoader.h"
+#include "FastCaloSimConverter/HitConverter.h"
 
 // ROOT headers
 #include "ROOT/TBufferMerger.hxx"
@@ -57,11 +58,14 @@ auto main(int argc, char* argv[]) -> int
         new TTree("FCS_ParametrizationInput", "FCS_ParametrizationInput");
 
     // Create a thread-local ExtrapolationConverter
-    ExtrapolationConverter converter(n_layers, &reader, outTree);
+    ExtrapolationConverter extrap_converter(n_layers, &reader, outTree);
+    HitConverter hit_converter(dd4hep_input, outTree);
 
     // Process entries in this thread's chunk
     while (reader.Next()) {
-      converter.process_entry();
+      Long64_t entryIndex = reader.GetCurrentEntry();
+      extrap_converter.process_entry();
+      hit_converter.process_entry(entryIndex);
       outTree->Fill();
     }
 
