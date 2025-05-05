a9f8ee8642057ce81d4511b4bccfcd0d322b35d7
Joshua Beirer <39070576+jbeirer@users.noreply.github.com>
2025-05-05 20:03:16 +0200

    Add valgrind test option (#20)

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -2,10 +2,8 @@ function(add_tests TEST_SOURCES)
     foreach(TEST_FILE ${TEST_SOURCES})
         get_filename_component(TEST_NAME ${TEST_FILE} NAME_WE)
 
-        # Add the test executable
         add_executable(${TEST_NAME} ${TEST_FILE})
 
-        # Link the test executable with the GoogleTest and necessary libraries
         target_link_libraries(${TEST_NAME} PRIVATE
             gtest
             gtest_main
@@ -13,25 +11,34 @@ function(add_tests TEST_SOURCES)
             ROOT::Core
             FastCaloSim::FastCaloSim)
 
-        # Add include directories
-        target_include_directories(${TEST_NAME} PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/include ${CMAKE_CURRENT_SOURCE_DIR})
+        target_include_directories(${TEST_NAME} PRIVATE
+            ${CMAKE_CURRENT_SOURCE_DIR}/include
+            ${CMAKE_CURRENT_SOURCE_DIR})
 
-        # Add the test to the test suite
-        add_test(NAME ${TEST_NAME}
-            COMMAND ${TEST_NAME})
-
-        # Set the test properties
-        # Propagate the dd4hep environment to tets
-        # Preload the DDFastCaloSim library
+        # Build the env var list once
         set(ENV_VARS
             "TEST_BASE_DIR=${TEST_BASE_DIR}"
             "FastCaloSim_LIB=${FastCaloSim_LIB}"
             "FastCaloSimParam_LIB=${FastCaloSimParam_LIB}"
             "DDFastCaloSim_LIB=${DDFastCaloSim_LIB}"
-            "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$ENV{LD_LIBRARY_PATH}"
+            "LD_LIBRARY_PATH=${CMAKE_BINARY_DIR}:$ENV{LD_LIBRARY_PATH}"
             "PYTHONPATH=${CMAKE_CURRENT_SOURCE_DIR}:$ENV{PYTHONPATH}"
         )
 
-        set_tests_properties(${TEST_NAME} PROPERTIES ENVIRONMENT "${ENV_VARS}")
+        if(ENABLE_VALGRIND_TESTS)
+            # Format env for inline shell command
+            string(REPLACE ";" " " ENV_STRING "${ENV_VARS}")
+            string(APPEND ENV_STRING " LD_PRELOAD=${DDFastCaloSim_LIB}")
+
+            # Use a single-line shell with env and valgrind
+            add_test(NAME ${TEST_NAME}
+                COMMAND /bin/bash -c
+                "${ENV_STRING} ${VALGRIND_EXECUTABLE} --tool=massif --pages-as-heap=yes --massif-out-file=${TEST_NAME}.massif $<TARGET_FILE:${TEST_NAME}>"
+            )
+        else()
+            add_test(NAME ${TEST_NAME}
+                COMMAND $<TARGET_FILE:${TEST_NAME}>)
+            set_tests_properties(${TEST_NAME} PROPERTIES ENVIRONMENT "${ENV_VARS}")
+        endif()
     endforeach()
 endfunction()
